1b33d2a93f95f3ac4b5c25d322780e38cd48f4a2
onnu514 <onyujj0514@gmail.com>
2026-04-30 14:16:46 +0900

    동기에서 비동기로 수정

--- backend_core/src/main/java/com/riskdetector/riskdetector/util/LambdaUtil.java
+++ backend_core/src/main/java/com/riskdetector/riskdetector/util/LambdaUtil.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.InvocationType;
 import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 import software.amazon.awssdk.services.lambda.model.InvokeResponse;
 
@@ -19,6 +20,20 @@ public class LambdaUtil {
         String payloadJson = objectMapper.writeValueAsString(payload);
         return lambdaClient.invoke(InvokeRequest.builder()
                 .functionName(functionName)
+                .payload(SdkBytes.fromUtf8String(payloadJson))
+                .build());
+    }
+
+    /**
+     * 비동기(Fire-and-forget) 방식으로 Lambda를 호출합니다.
+     * InvocationType=Event: AWS가 즉시 202를 반환하고 Lambda는 백그라운드에서 실행됩니다.
+     * Lambda 실행 완료 후 Destination(SQS)으로 결과가 자동 전송됩니다.
+     */
+    public void invokeAsync(String functionName, Object payload) throws Exception {
+        String payloadJson = objectMapper.writeValueAsString(payload);
+        lambdaClient.invoke(InvokeRequest.builder()
+                .functionName(functionName)
+                .invocationType(InvocationType.EVENT)
                 .payload(SdkBytes.fromUtf8String(payloadJson))
                 .build());
     }
